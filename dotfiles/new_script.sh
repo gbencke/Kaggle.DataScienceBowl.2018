d046460995c90abc49f22dfb0296cc7e1e83714a
Guilherme Bencke <gbencke@gmail.com>
2018-02-04 20:41:38 -0200

    Added libhdf5

--- dotfiles/new_script.sh
+++ dotfiles/new_script.sh
@@ -4,18 +4,12 @@ sudo apt-get update;
 
 sudo apt-get install -y net-tools git build-essential curl tmux vim mc tig openssh* 
 sudo apt-get install -y p7zip-full htop vim mc tig git make gcc curl tmux wget python-pip 
-sudo apt-get install -y ncurses-dev tree python-dev nano dos2unix bc
+sudo apt-get install -y ncurses-dev tree python-dev nano dos2unix bc libhdf5-dev    
 sudo apt-get install -y python3 python3-pip python3-dev cmake graphviz
 
 sudo apt-get install language-pack-en-base 
 sudo dpkg-reconfigure locales
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash ;
-
-nvm install v4.6.0
-nvm use v4.6.0
-
-npm install -g bower sass js-beautify typescript-formatter 
 
 mkdir git
 cd git
@@ -23,7 +17,7 @@ cd git
 sudo pip install autopep8 pylint virtualenv pmm
 sudo pip3 install autopep8 pylint virtualenv pmm
 
-
+wget https://s3.amazonaws.com/gbencke.kaggle/data.kaggle.bowl.7z
 
 
 
