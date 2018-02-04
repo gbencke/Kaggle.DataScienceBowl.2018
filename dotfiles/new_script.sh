ff92f53997e5f6a9a43f99844dbd37e1ad679866
Guilherme Bencke <gbencke@gmail.com>
2018-02-04 20:45:06 -0200

    Added more H5

--- dotfiles/new_script.sh
+++ dotfiles/new_script.sh
@@ -5,7 +5,7 @@ sudo apt-get update;
 sudo apt-get install -y net-tools git build-essential curl tmux vim mc tig openssh* 
 sudo apt-get install -y p7zip-full htop vim mc tig git make gcc curl tmux wget python-pip 
 sudo apt-get install -y ncurses-dev tree python-dev nano dos2unix bc libhdf5-dev    
-sudo apt-get install -y python3 python3-pip python3-dev cmake graphviz
+sudo apt-get install -y python3 python3-pip python3-dev cmake graphviz python-h5py
 
 sudo apt-get install language-pack-en-base 
 sudo dpkg-reconfigure locales
