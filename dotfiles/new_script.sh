d94ecfe7aa7bf8041d1d79c66a322c5b719d9675
Guilherme Bencke <gbencke@gmail.com>
2018-03-03 17:58:42 -0300

    Removal of creation of git folder

--- dotfiles/new_script.sh
+++ dotfiles/new_script.sh
@@ -11,9 +11,6 @@ sudo apt-get install -y language-pack-en-base
 sudo dpkg-reconfigure locales
 
 
-mkdir git
-cd git
-
 git config --global user.email "gbencke@benckesoftware.com.br"  
 git config --global user.name "Guilherme Bencke"  
 
